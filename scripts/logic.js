487dcd17ca8c63247d35806b6bb257f18bf8ceb4
Mayo <mayolito.waje08@gmail.com>
2022-10-25 14:52:57 +0800

    refactor getComputerChoice and implement playARound

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,19 +1,31 @@
-const choices = {
-  rock: 'scissors', // key win to value
-  paper: 'rock',
-  scissors: 'paper'
-};
-
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 const getComputerChoice = () => {
-  const picks = Object.keys(choices);
+  const picks = ['Rock', 'Paper', 'Scissors'];
   const randomIndex = getRandomInt(0, picks.length);
   
   return picks[randomIndex];
+};
+
+const playARound = (playerSelection, computerSelection) => {
+  const winningPicks = {
+    Rock: 'Scissors', // key win to value
+    Paper: 'Rock',
+    Scissors: 'Paper'
+  };
+
+  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+  if (winningPicks[playerSelection] === computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (winningPicks[computerSelection] === playerSelection) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return "It's a tie!";
+  }
 };
